6b1603a052fc7fce6d82349c1b0396975f7c575c
jayant-01 <01agarwal.jayant@gmail.com>
2023-02-21 03:28:39 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    // ignore: prefer_const_constructors
+    MaterialApp(
+      // ignore: prefer_const_constructors
+      home: Center(
+        child: const Text('Hello World!'),
+    ),
+  ),
+  );
+}
+
+
